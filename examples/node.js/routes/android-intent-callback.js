34c8f264829f827c90661224eeb1568f3eb020a6
Pertti Kroger <pertti@qvik.fi>
2020-03-27 16:13:18 +0200

    Add improved paymentUrl endpoint for iOS.
    Also removed unnecessary parameter from Android paymentUrl endpoint.

--- examples/node.js/routes/android-intent-callback.js
+++ examples/node.js/routes/android-intent-callback.js
@@ -3,11 +3,7 @@
 const { Joi } = require('celebrate');
 
 module.exports.schema = Joi.object().keys({
-    package: Joi.string().required(),
-    port: Joi.number()
-        .integer()
-        .min(1)
-        .max(65535)
+    package: Joi.string().required()
 }).unknown(true);
 
 module.exports.route = (req, res) => {
@@ -16,9 +12,6 @@ module.exports.route = (req, res) => {
     // request. This makes it so that we only ever need to compare
     // against paymentUrl in the SDK, and not do any further
     // interpretations of the url there.
-    const query = req.query;
-    const port = query.port;
-    const host = port ? `${req.hostname}:${port}` : req.hostname;
-    const intentUrl = `intent://${host}${req.originalUrl}#Intent;scheme=${req.protocol};action=com.swedbankpay.mobilesdk.VIEW_PAYMENTORDER;package=${encodeURIComponent(req.query.package)};end;`;
+    const intentUrl = `intent://${req.headers.host}${req.originalUrl}#Intent;scheme=${req.protocol};action=com.swedbankpay.mobilesdk.VIEW_PAYMENTORDER;package=${encodeURIComponent(req.query.package)};end;`;
     res.status(301).set('Location', intentUrl).send().end();
 };
