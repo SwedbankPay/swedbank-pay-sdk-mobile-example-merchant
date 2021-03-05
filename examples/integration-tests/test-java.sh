af98130d44995c2889dc6534f3beef324255bcbf
Johan Nordenswan <johan.nordenswan@qvik.com>
2021-03-05 11:25:32 +0100

    Workflow tuning

--- examples/integration-tests/test-java.sh
+++ examples/integration-tests/test-java.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 cd ../java/merchant
 ./gradlew bootRun --args ' --api-key='"$API_KEY"' --merchant-token='"$MERCHANT_TOKEN"' --merchant-id='"$MERCHANT_ID"'' &
 APP_PID=$!
@@ -7,6 +7,6 @@ cd -
 sleep 5
 until curl localhost:8080/healthcheck; do sleep 2; done
 
-curl -X POST -d @requests/paymentOrderRequest.json -H "Content-Type: application/json" -H "x-payex-sample-apikey: $API_KEY" localhost:8080/paymentorders | jq .
+curl -s --show-error --fail -X POST -d @requests/paymentOrderRequest.json -H "Content-Type: application/json" -H "x-payex-sample-apikey: $API_KEY" localhost:8080/paymentorders | jq .
 
 kill $APP_PID
