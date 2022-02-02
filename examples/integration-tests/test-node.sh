5a1afacdd488a97e8728c534b7f45dc6df105b61
Olof Thorén <olof.thoren@qvik.fi>
2022-02-02 16:15:08 +0100

    workflow should report error when failing

--- examples/integration-tests/test-node.sh
+++ examples/integration-tests/test-node.sh
@@ -8,6 +8,22 @@ node ./app.js &
 APP_PID=$!
 cd -
 sleep 5
-curl -v -s --show-error -X POST -d @requests/paymentOrderRequest_v1.json -H "Content-Type: application/json" -H "x-payex-sample-apikey: $API_KEY" -H "x-payex-sample-access-token: doot_doot" localhost:8080/paymentorders | jq .
+result=$(curl -v -s --show-error -X POST -d @requests/paymentOrderRequest_v1.json -H "Content-Type: application/json" -H "x-payex-sample-apikey: $API_KEY" -H "x-payex-sample-access-token: doot_doot" localhost:8080/paymentorders)
+status=$(echo $result | jq .status)
 
+echo $result | jq .
 kill $APP_PID
+
+if [ $status == "null" ]; then
+		# things are ok (I think...)
+		echo "No error status reported"
+		exit 0
+fi
+
+if [ $status -gt 299 ]
+     then
+     echo "Request reported error"
+     exit 1
+ 	 else
+ 	 echo "Request worked"
+fi
