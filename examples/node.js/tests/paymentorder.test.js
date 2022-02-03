cb3901c097c5b5327223d16e7a754dd2ca37a39e
Olof Thorén <olof.thoren@qvik.fi>
2022-02-03 14:21:01 +0100

    Codacy fixes

--- examples/node.js/tests/paymentorder.test.js
+++ examples/node.js/tests/paymentorder.test.js
@@ -1,7 +1,7 @@
 const constants = require('../util/constants.js');
-const app = require('../app.js')
-const chai = require('chai')
-const chaiHttp = require('chai-http')
+const app = require('../app.js');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const fs = require("fs");
 
 // Configure chai
@@ -13,13 +13,13 @@ let headers = {
   Accept: 'application/json', 
   [constants.apiKeyHeaderName]: [env["API_KEY"]],
   [constants.accessTokenHeaderName]: [env["MERCHANT_TOKEN"]],
-}
+};
 
 describe('Post PaymentOrder v2', () => {
   
   it('Payment order should be accepted', (done) => {
 
-    const paymentOrderV2 = JSON.parse(fs.readFileSync("tests/paymentOrderRequest_v2.json").toString())
+    const paymentOrderV2 = JSON.parse(fs.readFileSync("tests/paymentOrderRequest_v2.json").toString());
     chai.request(app)
       .post('/paymentorders')
       .set(headers)
@@ -32,4 +32,4 @@ describe('Post PaymentOrder v2', () => {
     
   })
   .timeout(15 * 1000);  //usually it never takes more than one second
-})
+});
