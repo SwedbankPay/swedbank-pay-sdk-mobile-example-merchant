c183cd32493b875ff8f8594f8f2add30659cd6d0
Olof Thorén <olof.thoren@qvik.fi>
2022-03-17 11:18:48 +0100

    Better UserAgent handling to report SDK version
    Send in the User-Agent as a regular http header in every call to Swedbank, relaying the header from the caller so we always send in the current value.

--- examples/node.js/tests/patch.test.js
+++ examples/node.js/tests/patch.test.js
@@ -14,13 +14,14 @@ chai.should();
 
 const env = process.env;
 let headers = { 
-  Accept: 'application/json', 
-  [constants.apiKeyHeaderName]: [env["API_KEY"]],
-  [constants.accessTokenHeaderName]: "doot_doot",
+	"User-Agent": "NodeTestSuite/3.0",
+	Accept: 'application/json', 
+	[constants.apiKeyHeaderName]: [env["API_KEY"]],
+	[constants.accessTokenHeaderName]: "doot_doot",
 };
 
 function checkCredentials(res) {
-  chai.assert(res.status != 401, "Getting 401, is the credentials missing?\n" + res.text);
+	chai.assert(res.status != 401, "Getting 401, is the credentials missing?\n" + res.text);
 }
 
 function printResult(res) {
@@ -28,107 +29,103 @@ function printResult(res) {
 }
 
 describe('Patch Instrument v3', () => {
-  
-  it('Payment order should be patched with a new Instrument', (done) => {
 
-	const paymentOrder = JSON.parse(fs.readFileSync("tests/paymentOrderRequest_v3.json").toString());
-	paymentOrder.paymentorder.instrument = "CreditCard";
+	it('Payment order should be patched with a new Instrument', (done) => {
 
-	//paymentOrder.paymentorder.generateRecurrenceToken = true;
-  //paymentOrder.paymentorder.generateUnscheduledToken = true;
-  // note that tokens are not compatible with all instruments
+		const paymentOrder = JSON.parse(fs.readFileSync("tests/paymentOrderRequest_v3.json").toString());
+		paymentOrder.paymentorder.instrument = "CreditCard";
+
+		//paymentOrder.paymentorder.generateRecurrenceToken = true;
+	  	//paymentOrder.paymentorder.generateUnscheduledToken = true;
+	  	// note that tokens are not compatible with all instruments
 
 
-	chai.request(app)
-	  .post('/paymentorders')
-	  .set(headers)
-	  .send(paymentOrder)
-	  .end((err, res) => {
+	    chai.request(app)
+	    .post('/paymentorders')
+	    .set(headers)
+	    .send(paymentOrder)
+	    .end((err, res) => {
 
-		checkCredentials(res);
-		
-		res.should.have.status(200);
-		res.body.should.be.a('object');
+		  	checkCredentials(res);
 
-		const href = findOperation(res.body, "set-instrument").href;
-		if (!href) {
-			console.log("error! No operation!");
-		}
-
-		//Now patch this payment order!
-		let params = {
-			href: href,
-			paymentorder: {
-				operation: "SetInstrument",
-				instrument: "Swish"
-			}
-		};
-
-		chai.request(app)
-		  .patch('/patch')
-		  .set(headers)
-		  .send(params)
-		  .end((err, res) => {
-
-		  	//console.log(res.text) 
-		  	//console.log(err)
 		  	res.should.have.status(200);
+		  	res.body.should.be.a('object');
 
-			done();
+		  	const href = findOperation(res.body, "set-instrument").href;
+		  	if (!href) {
+		  		console.log("error! No operation!");
+		  	}
+
+			//Now patch this payment order!
+			let params = {
+				href: href,
+				paymentorder: {
+					operation: "SetInstrument",
+					instrument: "Swish"
+				}
+			};
+
+			chai.request(app)
+			.patch('/patch')
+			.set(headers)
+			.send(params)
+			.end((err, res) => {
+
+				//console.log(res.text) 
+				//console.log(err)
+				res.should.have.status(200);
+				done();
+			});
 		});
-	 });
-	
-  })
-  .timeout(15 * 1000);  //usually it never takes more than one second
+	})
+  	.timeout(15 * 1000);  //usually it never takes more than one second
 });
 
 describe('Patch Abort', () => {
-  
-  it('Payment order should be aborted', (done) => {
 
-	const paymentOrder = JSON.parse(fs.readFileSync("tests/paymentOrderRequest_v3.json").toString());
+	it('Payment order should be aborted', (done) => {
 
-	chai.request(app)
-	  .post('/paymentorders')
-	  .set(headers)
-	  .send(paymentOrder)
-	  .end((err, res) => {
-
-		checkCredentials(res);
-		
-		res.should.have.status(200);
-		res.body.should.be.a('object');
-
-		const href = findOperation(res.body, "abort").href;
-		if (!href) {
-			console.log("error! No operation!");
-		}
-
-		//Now patch this payment order!
-		let params = {
-			href: href,
-			paymentorder: {
-				operation: "Abort",
-				abortReason: "AbortedByUser"
-			}
-		};
+		const paymentOrder = JSON.parse(fs.readFileSync("tests/paymentOrderRequest_v3.json").toString());
 
 		chai.request(app)
-		  .patch('/patch')
-		  .set(headers)
-		  .send(params)
-		  .end((err, res) => {
+		.post('/paymentorders')
+		.set(headers)
+		.send(paymentOrder)
+		.end((err, res) => {
 
-		  	//console.log(res.text) 
-		  	//console.log(err)
-		  	res.should.have.status(200);
+			checkCredentials(res);
 
-			done();
+			res.should.have.status(200);
+			res.body.should.be.a('object');
+
+			const href = findOperation(res.body, "abort").href;
+			if (!href) {
+				console.log("error! No operation!");
+			}
+
+			//Now patch this payment order!
+			let params = {
+				href: href,
+				paymentorder: {
+					operation: "Abort",
+					abortReason: "AbortedByUser"
+				}
+			};
+
+			chai.request(app)
+			.patch('/patch')
+			.set(headers)
+			.send(params)
+			.end((err, res) => {
+
+				//console.log(res.text) 
+				//console.log(err)
+				res.should.have.status(200);
+				done();
+			});
 		});
-	 });
-	
-  })
-  .timeout(15 * 1000);  //usually it never takes more than one second
+	})
+  	.timeout(15 * 1000);  //usually it never takes more than one second
 });
 
 /*
@@ -146,7 +143,7 @@ describe('Verify and get tokens', () => {
 	  .send({ resource, expand })
 	  .end((err, res) => {
 
-	  	printResult(res)
+		printResult(res)
 
 			checkCredentials(res);
 			
